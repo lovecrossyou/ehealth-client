e81999341628abaac94911d9176e614c19328dec
xiannvrong <1796940658@qq.com>
2019-05-30 09:26:37 +0800

    xx

--- pages/moment/moment.vue
+++ pages/moment/moment.vue
@@ -1,6 +1,10 @@
 <template>
-	<view>
-		
+	<view class="wrapper">
+		<!-- 头部 -->
+		<view class="top_area">
+			<view>动态</view>
+			<view>+ 发布</view>
+		</view>
 	</view>
 </template>
 
@@ -18,5 +22,18 @@
 </script>
 
 <style>
-
+.wrapper{
+	width: 100%;
+	padding: 31upx;
+	box-sizing: border-box;
+}
+.top_area{
+	width: 100%;
+	height: 130upx;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	background-color: pink;
+}
 </style>

e3fb460afe53775537bc076c67727091b9f0fa58
xiannvrong <1796940658@qq.com>
2019-07-18 09:12:05 +0800

    xx

---
+++ pages/learning/conferenceDetails.vue
@@ -0,0 +1,242 @@
+<template>
+	<view class="wrapper">
+		<!-- 头部banner -->
+		<image src="http://qnimage.xiteng.com/tu.png" mode="aspectFill" class="top_banner_img"></image>
+		<!-- 主要内容 -->
+		<view class="main_content">
+			<!-- 会议基本信息 -->
+			<view class="conference_basic_info">
+				<view class="conference_name">2019中国康养产业创新发展论坛</view>
+				<view class="conference_browse">
+					<view class="browse_srea">
+						<image src="http://qnimage.xiteng.com/icon_pageview@2x.png" class="browse_icon"></image>
+						<view class="browse_num">浏览 1897</view>
+					</view>
+					<view class="browse_srea">
+						<image src="http://qnimage.xiteng.com/icon_like@2x.png" class="browse_icon"></image>
+						<view class="browse_num">收藏 49</view>
+					</view>
+				</view>
+				<view class="conference_tickets_price">
+					<view class="rmb_symbol">¥</view>
+					<view class="price_num">50</view>
+				</view>
+				<view class="conference_time">
+					<image src="http://qnimage.xiteng.com/icon_time@2x.png" class="browse_icon"></image>
+					<view class="conference_time_text">2019.09.10  周六  13:30-17:00</view>
+				</view>
+				<view class="conference_time">
+					<image src="http://qnimage.xiteng.com/dingwei@2x.png" class="browse_icon"></image>
+					<view class="conference_time_text">北京西城宣武门外大街甲1环球财讯中心</view>
+					<image src="http://qnimage.xiteng.com/image_location@2x.png" class="right_icon"></image>
+				</view>
+			</view>
+			<!-- 中国老年和老年医学学会康养分会 -->
+			<view class="go_affiliate">
+				<image src="http://img2.imgtn.bdimg.com/it/u=2664341930,2707281610&fm=26&gp=0.jpg" mode="aspectFill" class="user_img"></image>
+				<view class="center_cont">
+					<view class="affiliate_name">中国老年和老年医学学会康养分会</view>
+					<view class="attend_meetings_info">
+						<view class="meeting_num">300+会议</view>
+						<view class="join_meeting_num">5000+参加</view>
+					</view>
+				</view>
+				<image src="http://qnimage.xiteng.com/arrow_right@2x.png" class="go_next_icon"></image>
+			</view>
+			<!-- 活动详情 -->
+			<view class="activity_details">
+				<view class="top_title">活动详情</view>
+				<view class="activity_progress">
+					<view class="activity_progress_num">01</view>
+					<view class="activity_progress_text">活动介绍</view>
+				</view>
+				<view class="activity_details_content">
+					进入21世纪以来，我国的经济高速发展，在短短的十几年的时间内就已经成为世界一流经济强国，但是在经济快速的同时，也伴随着些许的问题，人口老龄化就是其中较为突出的问题之一，
+					根据预测据预测，到2050年中国老年人口将增至4.8亿，占全球老年人口的四分之一，为此国家频繁布局，提出了以“健康中国”为发展核心的战略部署，并出台了一系列鼓励有关康养产业的证政策，
+					因此我国诞生了一大批以康养为宗旨的产业。
+				</view>
+				<view class="activity_details_img">
+					
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+	methods: {}
+};
+</script>
+
+<style scoped>
+.wrapper {
+	width: 100%;
+}
+.top_banner_img {
+	width: 100%;
+	height: 362upx;
+}
+.main_content{
+	width: 100%;
+}
+.conference_basic_info{
+	width: 100%;
+	padding: 25upx 33upx 28upx 33upx;
+	box-sizing: border-box;
+	border-bottom: 20upx solid #F4F6F6;
+}
+.conference_name{
+	color: #333333;
+	font-size: 32upx;
+	font-weight:500;
+}
+.conference_browse{
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	margin-top: 18upx;
+}
+.browse_icon{
+	width: 36upx;
+	height: 36upx;
+	margin-right: 10upx;
+}
+.browse_num{
+	color: #999999;
+	font-size: 24upx;
+}
+.browse_srea{
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	margin-right: 30upx;
+}
+.conference_tickets_price{
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	color: #FF6F28;
+	margin-top: 25upx;
+	font-weight:400;
+	padding-bottom: 25upx;
+	box-sizing: border-box;
+	border-bottom: 1upx solid #E3E3E3;
+	margin-bottom: 30upx;
+}
+.rmb_symbol{
+	font-size: 26upx;
+	margin-right: 10upx;
+	margin-top: 5upx;
+}
+.price_num{
+	font-size: 34upx;
+}
+.conference_time{
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	margin-top: 13upx;
+}
+.conference_time_text{
+	color: #333333;
+	font-size: 28upx;
+	flex: 1;
+}
+.right_icon{
+	width: 52upx;
+	height: 52upx;
+}
+.go_affiliate{
+	width: 100%;
+	padding: 29upx 30upx;
+	box-sizing: border-box;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	border-bottom: 20upx solid #F4F6F6;
+}
+.user_img{
+	width:112upx;
+	height:112upx;
+	border-radius:10upx;
+}
+.center_cont{
+	flex: 1;
+	margin-left: 25upx;
+}
+.affiliate_name{
+	color: #333333;
+	font-size: 28upx;
+}
+.attend_meetings_info{
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	margin-top: 20upx;
+}
+.go_next_icon{
+	width: 30upx;
+	height: 30upx;
+}
+.meeting_num,
+.join_meeting_num{
+	width:140upx;
+	color: #999;
+	font-size: 24upx;
+}
+.meeting_num{
+	border-right: 1upx solid #999999;
+	margin-right: 30upx;
+}
+.activity_details{
+	width: 100%;
+	padding: 47rpx 33rpx;
+	box-sizing: border-box;
+}
+.top_title{
+	color: #333333;
+	font-size: 32upx;
+}
+.activity_progress{
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	margin-top: 50rpx;
+}
+.activity_progress_num{
+	width: 52upx;
+	height: 40upx;
+	border-top:5rpx solid #CDB675;
+	border-right:12rpx solid #CDB675;
+	border-bottom:5rpx solid #CDB675;
+	border-left:5rpx solid #CDB675;
+	color: #CDB675;
+	font-size: 28upx;
+	text-align: center;
+	font-weight: 500;
+	line-height: 40upx;
+}
+.activity_progress_text{
+	flex: 1;
+	height: 45upx;
+	margin-left: 20upx;
+	color: #CDB675;
+	font-size: 28upx;
+	font-weight: 500;
+	line-height: 45upx;
+	border-bottom:2rpx solid #CDB675;
+}
+.activity_details_content{
+	color: #333333;
+	font-size: 30upx;
+	margin-top: 25upx;
+	line-height: 50upx;
+}
+</style>
+
--- pages/learning/jiaoliu.vue
+++ pages/learning/jiaoliu.vue
@@ -1,208 +1,192 @@
 <template>
-	<div class="container">
-		<div class="list">
-			<div class="list-item">
-				<div class="list-item-bg">
+	<view class="container">
+		<view class="list">
+			<view class="list-item" @click="goConferenceDetails">
+				<view class="list-item-bg">
 					<image src="http://qnimage.xiteng.com/tu.png" class="bg-image" mode="scaleToFill"></image>
-					<div class="list-item-bg-tips">可报名</div>
+					<view class="list-item-bg-tips">可报名</view>
 					<image src="../../static/learn/icon_time.png" class="list-item-bg-clock"></image>
-					<div class="time">
-						2019-06-13 10：00
-					</div>
-					<div class="list-item-bg-button">立即报名</div>
-				</div>
-				<div class="title">
-					2019第一届国际康养博览会
-				</div>
-				<div class="footer">
+					<view class="time">2019-06-13 10：00</view>
+					<view class="list-item-bg-button">立即报名</view>
+				</view>
+				<view class="title">2019第一届国际康养博览会</view>
+				<view class="footer">
 					<image src="../../static/learn/icon_location.png" class="icon_location"></image>
-					<div class="addr">
-						国家会议中心
-					</div>
-				</div>
-			</div>
-			
-			<div class="list-item">
-				<div class="list-item-bg">
+					<view class="addr">国家会议中心</view>
+				</view>
+			</view>
+
+			<view class="list-item" @click="goConferenceDetails">
+				<view class="list-item-bg">
 					<image src="http://qnimage.xiteng.com/tu.png" class="bg-image" mode="scaleToFill"></image>
-					<div class="list-item-bg-tips">可报名</div>
+					<view class="list-item-bg-tips">可报名</view>
 					<image src="../../static/learn/icon_time.png" class="list-item-bg-clock"></image>
-					<div class="time">
-						2019-06-13 10：00
-					</div>
-					<div class="list-item-bg-button">立即报名</div>
-				</div>
-				<div class="title">
-					2019第一届国际康养博览会
-				</div>
-				<div class="footer">
+					<view class="time">2019-06-13 10：00</view>
+					<view class="list-item-bg-button">立即报名</view>
+				</view>
+				<view class="title">2019第一届国际康养博览会</view>
+				<view class="footer">
 					<image src="../../static/learn/icon_location.png" class="icon_location"></image>
-					<div class="addr">
-						国家会议中心
-					</div>
-				</div>
-			</div>
-			
-			<div class="list-item">
-				<div class="list-item-bg">
+					<view class="addr">国家会议中心</view>
+				</view>
+			</view>
+
+			<view class="list-item" @click="goConferenceDetails">
+				<view class="list-item-bg">
 					<image src="http://qnimage.xiteng.com/tu.png" class="bg-image" mode="scaleToFill"></image>
-					<div class="list-item-bg-tips">可报名</div>
+					<view class="list-item-bg-tips">可报名</view>
 					<image src="../../static/learn/icon_time.png" class="list-item-bg-clock"></image>
-					<div class="time">
-						2019-06-13 10：00
-					</div>
-					<div class="list-item-bg-button">立即报名</div>
-				</div>
-				<div class="title">
-					2019第一届国际康养博览会
-				</div>
-				<div class="footer">
+					<view class="time">2019-06-13 10：00</view>
+					<view class="list-item-bg-button">立即报名</view>
+				</view>
+				<view class="title">2019第一届国际康养博览会</view>
+				<view class="footer">
 					<image src="../../static/learn/icon_location.png" class="icon_location"></image>
-					<div class="addr">
-						国家会议中心
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+					<view class="addr">国家会议中心</view>
+				</view>
+			</view>
+		</view>
+	</view>
 </template>
 
 <script>
-	export default {
-
+export default {
+	methods:{
+		goConferenceDetails(){
+			uni.navigateTo({
+				url:'/pages/learning/conferenceDetails'
+			})
+		}
 	}
+};
 </script>
 
 <style scoped>
-	.container {
-		margin: auto;
-	}
-	
-	.bg-image{
-		position: absolute;
-		left: 0;
-		right: 0;
-		width: 690upx;
-		height: 360upx;
-		
-		padding-top: 30upx;
-		box-sizing: border-box;
-	}
+.container {
+	margin: auto;
+}
 
-	.list-item {
-		padding:  0  30upx 34upx 30upx;
-		box-sizing: border-box;
-		background: #FFFFFF;
-		border-bottom: solid 1upx #e1e1e1;
-		
-		margin-bottom: 30upx;
-	}
+.bg-image {
+	position: absolute;
+	left: 0;
+	right: 0;
+	width: 690upx;
+	height: 360upx;
 
-	.list-item-bg {
-		width: 100%;
-		height: 360upx;
-		position: relative;
-/* 
+	padding-top: 30upx;
+	box-sizing: border-box;
+}
+
+.list-item {
+	padding: 0 30upx 34upx 30upx;
+	box-sizing: border-box;
+	background: #ffffff;
+	border-bottom: solid 1upx #e1e1e1;
+
+	margin-bottom: 30upx;
+}
+
+.list-item-bg {
+	width: 100%;
+	height: 360upx;
+	position: relative;
+	/* 
 		display: flex;
 		justify-content: center;
 		align-items: center; */
 
-		/* background-color: #2829DB; */
-		border-radius: 10upx;
-		background-color: #FFFFFF;
+	/* background-color: #2829DB; */
+	border-radius: 10upx;
+	background-color: #ffffff;
+}
 
-	}
+.list-item-bg-tips {
+	position: absolute;
+	left: 0upx;
+	top: 40upx;
+	width: 87upx;
+	height: 42upx;
+	background: rgba(255, 168, 0, 1);
 
-	.list-item-bg-tips {
-		position: absolute;
-		left: 0upx;
-		top: 40upx;
-		width: 87upx;
-		height: 42upx;
-		background: rgba(255, 168, 0, 1);
+	color: #ffffff;
+	font-size: 24upx;
+	text-align: center;
+	border-radius: 10upx;
+	line-height: 42upx;
+}
 
-		color: #FFFFFF;
-		font-size: 24upx;
-		text-align: center;
-		border-radius: 10upx;
-		line-height: 42upx;
-	}
+.list-item-bg-button {
+	position: absolute;
+	right: 25upx;
+	bottom: 22upx;
+	width: 140upx;
+	height: 50upx;
+	background: rgba(255, 168, 0, 1);
 
-	.list-item-bg-button {
-		position: absolute;
-		right: 25upx;
-		bottom: 22upx;
-		width: 140upx;
-		height: 50upx;
-		background: rgba(255, 168, 0, 1);
+	color: #ffffff;
+	font-size: 24upx;
+	text-align: center;
+	border-radius: 10upx;
+	line-height: 50upx;
+}
 
-		color: #FFFFFF;
-		font-size: 24upx;
-		text-align: center;
-		border-radius: 10upx;
-		line-height: 50upx;
-	}
+.list-item-bg-title {
+	width: 690px;
+	height: 360px;
+	opacity: 0.8;
+	border-radius: 10px;
+	margin-top: 28upx;
+}
 
-	.list-item-bg-title {
-		width: 690px;
-		height: 360px;
-		opacity: 0.8;
-		border-radius: 10px;
-		margin-top: 28upx;
-	}
+.list-item-bg-clock {
+	position: absolute;
+	left: 17upx;
+	bottom: 23upx;
+	width: 40upx;
+	height: 40upx;
+}
 
-	.list-item-bg-clock {
-		position: absolute;
-		left: 17upx;
-		bottom: 23upx;
-		width: 40upx;
-		height: 40upx;
-	}
+.time {
+	position: absolute;
+	left: 65upx;
+	bottom: 23upx;
+	font-size: 26upx;
+	font-family: PingFangSC-Regular;
+	font-weight: 400;
+	color: rgba(255, 255, 255, 1);
+	/* background-color: #000000; */
+	/* line-height:44upx; */
+}
 
-	.time {
-		position: absolute;
-		left: 65upx;
-		bottom: 23upx;
-		font-size: 26upx;
-		font-family: PingFangSC-Regular;
-		font-weight: 400;
-		color: rgba(255, 255, 255, 1);
-		/* background-color: #000000; */
-		/* line-height:44upx; */
-	}
+.title {
+	font-size: 32upx;
+	font-family: PingFangSC-Regular;
+	font-weight: 400;
+	color: rgba(51, 51, 51, 1);
+	line-height: 44upx;
+	margin-top: 28upx;
+	background-color: #ffffff;
+}
 
-	.title {
+.footer {
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	margin-top: 22upx;
+	background-color: #ffffff;
+}
 
-		font-size: 32upx;
-		font-family: PingFangSC-Regular;
-		font-weight: 400;
-		color: rgba(51, 51, 51, 1);
-		line-height: 44upx;
-		margin-top: 28upx;
-		background-color: #FFFFFF;
+.icon_location {
+	width: 40upx;
+	height: 40upx;
+}
 
-	}
-
-	.footer {
-		display: flex;
-		flex-direction: row;
-		align-items: center;
-		margin-top: 22upx;
-		background-color: #FFFFFF;
-	}
-
-	.icon_location {
-		width: 40upx;
-		height: 40upx;
-	}
-
-	.addr {
-
-		font-size: 26upx;
-		font-family: PingFangSC-Regular;
-		font-weight: 400;
-		color: rgba(153, 153, 153, 1);
-		line-height: 44upx;
-		background-color: #FFFFFF;
-	}
+.addr {
+	font-size: 26upx;
+	font-family: PingFangSC-Regular;
+	font-weight: 400;
+	color: rgba(153, 153, 153, 1);
+	line-height: 44upx;
+	background-color: #ffffff;
+}
 </style>
